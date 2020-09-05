bb597edc0d8997f4a980caeb27889b6ab98ca3b4
Farhan Bin Amin <farhanbinamin@gmail.com>
2020-09-06 00:35:55 +0600

    grid, copy message & heading - all 3 UIs done

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        body: ['IBM Plex Serif'],
+        main_title: ['Poppins'],
+        sub_title: ['Space Mono']
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
