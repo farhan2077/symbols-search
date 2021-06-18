e11e138a9033ca306a63011e37d1dabdb814859f
Farhan Bin Amin <farhanbinamin@gmail.com>
2021-06-18 23:17:38 +0600

    fix: `public` folder restructure & related changes

--- pages/_document.js
+++ pages/_document.js
@@ -14,26 +14,49 @@ class MyDocument extends Document {
             rel="stylesheet"
           />
 
-          <link rel="icon" href="/static/favicons/favicon.ico" />
+          <link rel="icon" href="/favicons/favicon.ico" />
           <link
             rel="apple-touch-icon"
             sizes="180x180"
-            href="/static/apple-touch-icon.png"
+            href="/favicons/apple-touch-icon.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="32x32"
-            href="/static/favicon-32x32.png"
+            href="/favicons/favicon-32x32.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="16x16"
-            href="/static/favicon-16x16.png"
+            href="/favicons/favicon-16x16.png"
           />
-          <link rel="manifest" href="/static/site.webmanifest" />
+          <link rel="manifest" href="/site.webmanifest" />
           <meta name="msapplication-TileColor" content="#5EEAD4" />
+
+          <meta property="og:title" content="Symbols Search" />
+          <meta property="og:type" content="website" />
+          <meta
+            property="og:url"
+            content="https://symbols-search.vercel.app/"
+          />
+          <meta
+            property="og:description"
+            content="Find symbols from Microsoft Word equation easily."
+          />
+          <meta property="og:image:width" content="1200" />
+          <meta property="og:image:height" content="628" />
+          <meta property="og:image" content="/images/preview.png" />
+
+          <meta name="twitter:card" content="summary" />
+          <meta name="twitter:site" content="@farhanbinamin" />
+          <meta name="twitter:title" content="Symbols search" />
+          <meta
+            name="twitter:description"
+            content="Find symbols from Microsoft Word equation easily."
+          />
+          <meta name="twitter:image" content="/images/preview.png" />
         </Head>
         <body className="bg-white dark:bg-gray-900">
           <Main />
