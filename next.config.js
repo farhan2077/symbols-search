bc3794732edd47ffcfe415adb69e5e39e2201562
Farhan Bin Amin <farhanbinamin@gmail.com>
2021-06-21 17:48:44 +0600

    new: replace `react` with `preact` only in client prod build

--- next.config.js
+++ next.config.js
@@ -2,4 +2,16 @@ module.exports = {
   eslint: {
     dirs: ["pages", "utils", "components"],
   },
+  webpack: (config, { dev, isServer }) => {
+    // replace react with preact only in client prod build
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: "preact/compat",
+        "react-dom/test-utils": "preact/test-utils",
+        "react-dom": "preact/compat",
+      });
+    }
+
+    return config;
+  },
 };
