333d5583adc4ef75c502c556e76b69eabc9ad435
Farhan Bin Amin <farhanbinamin@gmail.com>
2021-07-06 17:09:09 +0600

    refactor: `Layout` component simplified

--- components/Layout.js
+++ components/Layout.js
@@ -1,16 +1,14 @@
-import React from "react";
-import Container from "./Container";
 import Header from "./Header";
 import Footer from "./Footer";
 
 export default function Layout({ children }) {
   return (
-    <Container>
-      <section className="flex flex-col min-h-screen">
+    <div className="px-2 md:container md:mx-auto md:px-12 lg:px-32">
+      <div className="flex flex-col min-h-screen">
         <Header />
         <main className="flex-1">{children}</main>
         <Footer />
-      </section>
-    </Container>
+      </div>
+    </div>
   );
 }
